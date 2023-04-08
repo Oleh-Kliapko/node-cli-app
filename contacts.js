5d520dc3b577b9ddde4652d59c6aeae31d05ed68
Oleh Kliapko <goldclub1977@gmail.com>
2023-04-08 09:23:19 +0300

    step 1 is done

---
+++ contacts.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+const contactsPath = path.resolve("./db/contacts.json");
+console.log("🚀 contactsPath:", contactsPath);
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
